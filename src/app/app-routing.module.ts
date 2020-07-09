220b371d355fb17f3cce73338261d59edb2827e2
Fernando Wibowo <30991688+fwibowo88@users.noreply.github.com>
2020-07-09 23:08:13 +0700

    Generating Intial Page for App
    -Generate Auth Page (Login)
    -Generate Bookings Page (List Booking)
    -Generate Places
     > Offer + New Offer + Booking Offer
     > Discover + Place Detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,28 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
+
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'places',
     pathMatch: 'full'
   },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthPageModule)
+  },
+  {
+    path: 'places',
+    loadChildren: () => import('./places/places.module').then( m => m.PlacesPageModule)
+  },
+  {
+    path: 'bookings',
+    loadChildren: () => import('./bookings/bookings.module').then( m => m.BookingsPageModule)
+  },
+  // {
+  //   path: 'discover',
+  //   loadChildren: () => import('./places/discover/discover.module').then( m => m.DiscoverPageModule)
+  // },
 ];
 
 @NgModule({
